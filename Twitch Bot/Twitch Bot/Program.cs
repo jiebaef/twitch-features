f843dd7aa16652274cd195fb31d3a354bb0c1484
Finn <zockeremail2@yahoo.de>
2020-12-26 12:54:59 +0100

    created twitch bot solution

---
+++ Twitch Bot/Twitch Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Twitch_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
